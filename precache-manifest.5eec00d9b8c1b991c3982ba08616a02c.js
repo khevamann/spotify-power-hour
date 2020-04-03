5ecc4e7120c8cde87e72ea8a3f4d90bff3f15c64
khevamann <khevamann@users.noreply.github.com>
2020-04-03 03:29:19 +0000

    deploy: 07bbf1308d2eb17b2fe341c934f61e13cf3f8534

---
+++ precache-manifest.5eec00d9b8c1b991c3982ba08616a02c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3a1a0a735c23410d06d6",
+    "url": "/static/js/main.84c9c2a7.chunk.js"
+  },
+  {
+    "revision": "d73b060bfdf63a77013e",
+    "url": "/static/js/2.0c8f3ae9.chunk.js"
+  },
+  {
+    "revision": "3a1a0a735c23410d06d6",
+    "url": "/static/css/main.204d09f5.chunk.css"
+  },
+  {
+    "revision": "7d4de705a85cc20d3e1d00bdcfbb0b72",
+    "url": "/index.html"
+  }
+];
