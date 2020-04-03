dc4d83b07c8496ea1f2be51acd906177748240db
kheva <khevamann@gmail.com>
2020-04-02 21:09:24 -0700

    Fixed redirect url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "d4450f0cf6874de186d3516d6dd095a8";
-export const redirectUri = "https://khevamann.com/spotify-power-hour/";
+export const redirectUri = "http://khevamann.com/spotify-power-hour/";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
