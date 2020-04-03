8b19ac8c41f1f4b4d3da1dbc74dcf8e46ff3d9b3
khevamann <khevamann@users.noreply.github.com>
2020-04-03 03:57:48 +0000

    deploy: 48ab4fb3791f98ad282869fc0d1d06a088ccc1ee

---
+++ precache-manifest.81389825a76e82dc515b687054924577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotify-power-hour/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eaa9b83e7625a497d215",
+    "url": "/spotify-power-hour/static/js/runtime~main.e1d5144b.js"
+  },
+  {
+    "revision": "e0bf8eb9a73cf838b841",
+    "url": "/spotify-power-hour/static/js/main.6600bb1e.chunk.js"
+  },
+  {
+    "revision": "d73b060bfdf63a77013e",
+    "url": "/spotify-power-hour/static/js/2.0c8f3ae9.chunk.js"
+  },
+  {
+    "revision": "e0bf8eb9a73cf838b841",
+    "url": "/spotify-power-hour/static/css/main.204d09f5.chunk.css"
+  },
+  {
+    "revision": "a36c124e1e060ac2e2f5fdafb7f1ac73",
+    "url": "/spotify-power-hour/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5eec00d9b8c1b991c3982ba08616a02c.js"
+  "/spotify-power-hour/precache-manifest.81389825a76e82dc515b687054924577.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/spotify-power-hour/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
